7e87d275301890ddbb3dadc061b1c8756f74881c
jahid45780 <jahidhossain88899@gmail.com>
2024-08-26 22:27:18 +0600

    add

---
+++ src/Componets/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+              <h1 className=" text-center font-bold text-lg text-white p-2" > Developed by Jahid </h1>     
+</footer>
+        </div>
+    );
+};
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './Style/Style.css'
 import Home from './Componets/Home/Home'
+import Footer from './Componets/Footer/Footer'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home></Home></StrictMode>,
+    <Home></Home>
+     <Footer/>
+    </StrictMode>,
 )
